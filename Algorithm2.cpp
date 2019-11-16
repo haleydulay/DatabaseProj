9410082d6c23d17d32f65125ae656a97706a101e
Franco Marcoccia <fm16@my.fsu.edu>
2019-11-16 17:28:34 -0500

    Create Algorithm2.cpp

---
+++ Algorithm2.cpp
@@ -0,0 +1,29 @@
+/*
+Input:
+C, the clustering result of the S-phase.
+0s, a threshold to determine whether to merge two records.
+Output:
+D, the final clustering result
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+vector<string> D; // D={}
+
+for(i=0;i<clusters.size;i++)
+{
+  Sc = {sC}   // pack its signature into a signature set
+  for(j=0;j<D.size;j++)
+   {
+   //compute simd(C,D)    dynamic similarity
+   }
+   if
+}
+
+return 0;   // return D
+}
