6fbe2909d633e6231c764504282cac32cfe8493e
haleydulay <43179228+haleydulay@users.noreply.github.com>
2019-12-02 15:32:27 -0500

    Add files via upload

--- algorithm1.cpp
+++ algorithm1.cpp
@@ -19,31 +19,46 @@ Sc, the set of cluster signatures corresponding to C, each cluster in C has a si
 
 using namespace std;
 
-double record_similarity(vector<string> c, vector<string> d)
+int placement;
+int maxval = 0;
+int counter = 0;
+
+double record_similarity(vector<string> c, vector<string> d, int j)
 {
+int checker = 0;
 double result = 0;
-        for(int a=0; a<c.size(); a++)
+        for(int a=0; a<c.size(); a++)		// looping through record's attributes
         {
-                for(int b=0; b<d.size(); b++)
+                for(int b=0; b<d.size(); b++)	// looping through signature's record's attributes
                 {
                         if(c[a]==d[b])
+			{
                                 result += 1;
+				checker += 1;
+			}
                 }
+		if(checker>maxval)
+		{
+			maxval = checker;
+			placement = j;
+		}
         }
+	checker = 0;
 
         return result / (c.size()+d.size());
 }
 
-double cluster_similarity(vector<string> r, vector<string> r2)
+double cluster_similarity(vector<string> r, vector<string> r2, int j)
 {
 double result = 0;
-result += record_similarity(r,r2);
+result += record_similarity(r,r2,j);
 
 	return result;
 }
 
 int main()
 {
+double threshold = 1000000000;
 ifstream file;
 int index = 0;
 clock_t beg = 0;
@@ -56,7 +71,7 @@ string t1 = "Matching-Accuracy";
 string t2 = "Run-Time";
 vector<string> Cmax;
 vector<string> Scmax;
-int storage = 0;
+double storage = 0;
 vector<vector<string> > C(738);
 vector<vector<string> > Sc(738);
 
@@ -93,38 +108,53 @@ if(experiment==t1)
     }
     file.close();
 
+/*
+    C[0].push_back("Feng Li");
+  C[0].push_back("Feng Li");
+counter++;
+counter++;
+counter ++;
+counter ++;
+ Sc[0].push_back("Feng Li");
+ Sc[0].push_back("Feng Lu");
+C[1].push_back("Feng Li");
+C[1].push_back("Feng Li");
+Sc[1].push_back("Sheldon Park");
+Sc[1].push_back("Wei Wang"); */
+
 
     for(int i=0;i<R.size();i++) // for all r in R do    //can change the size to DBLP-WW possibly if not exact # of records
     {
-        for(int j=0;j<C.size();j++)  // for all C in C do
-            storage += cluster_similarity(R[i],Sc[j]);
-	
-	if(storage!=0)
-		cout << storage / Sc.size();
-	if(i==1)
-		break;
-	
-    }
+    double test = 0;
+        for(int j=0;j<counter-1;j++)  	// for all C in C do
+            storage += cluster_similarity(R[i],Sc[j],j);
+  
+	//if(i==1)
+	//break;
 
-/*
-        if(//maxC in csims(r,C)>=0s   if true, then merge to the next possible cluster)
+	test = cluster_similarity(R[i],Sc[placement],i);
+
+	cout << test << endl;	
+
+        if(test>=threshold)
         {
-            //Cmax = arg max C in c sims(r,C)
-            Cmax.push_back(R[i]);           //Cmax <- Cmax union r          append r to Cmax
+            //Cmax = arg max C in c sims(r,C) = placement
+            for(int k=0;k<R[i].size();k++)
+	    	C[placement].push_back(R[i][k]);           //Cmax <- Cmax union r          append r to Cmax
             //r1 <- arg max r in C r.t     O(1) since sorted by time
-            Scmax.pop_back();                //sCmax <- sCmax \ r1           remove the last record
-            Scmax.push_back(R[i]);          //sCmax <- sCmax unior r        append r as the last record
+            Sc[placement].pop_back();                //sCmax <- sCmax \ r1           remove the last record
+            Sc[placement].push_back(R[i][R[i].size()-1]);          //sCmax <- sCmax union r        append r as the last record
         }
         else
         {      
             Cmax = R[i];           // create a new cluster             cmax = {R}
             Scmax = R[i];           // sCmax = {r}          // initialize the signature for Cmax
-            out.C[i].push_back(Cmax);                       // C <- C union Cmax
-            out.Sc[i].push_back(Scmax);                       // Sc <- Sc union sCmax
+            C[i].push_back(Cmax);                       // C <- C union Cmax
+            Sc[i].push_back(Scmax);                       // Sc <- Sc union sCmax
         }   
     }
-    return out;       // return C, Sc
-*/
+
+
 return 0;
 }
 // -------------------------------------------------------------------------------------------------------------------
