0f6ab36ae23f490bacc7274adc27b7108d17ccd8
haleydulay <43179228+haleydulay@users.noreply.github.com>
2019-12-02 13:13:28 -0500

    updated algorithm

--- algorithm1.cpp
+++ algorithm1.cpp
@@ -19,11 +19,28 @@ Sc, the set of cluster signatures corresponding to C, each cluster in C has a si
 
 using namespace std;
 
-struct output
+double record_similarity(vector<string> c, vector<string> d)
 {
-vector<string> C;
-vector<string> Sc;
-};
+double result = 0;
+        for(int a=0; a<c.size(); a++)
+        {
+                for(int b=0; b<d.size(); b++)
+                {
+                        if(c[a]==d[b])
+                                result += 1;
+                }
+        }
+
+        return result / (c.size()+d.size());
+}
+
+double cluster_similarity(vector<string> r, vector<string> r2)
+{
+double result = 0;
+result += record_similarity(r,r2);
+
+	return result;
+}
 
 int main()
 {
@@ -37,9 +54,12 @@ string line;
 string experiment;
 string t1 = "Matching-Accuracy";
 string t2 = "Run-Time";
-output out;
 vector<string> Cmax;
 vector<string> Scmax;
+int storage = 0;
+vector<vector<string> > C(738);
+vector<vector<string> > Sc(738);
+
 
 cout << "Input either 'Matching-Accuracy' or 'Run-Time' as the experiment you want to conduct: ";
 cin >> experiment;
@@ -57,18 +77,18 @@ if(experiment==t1)
     {
         while(!file.eof())
         {
-		if(file.peek()!='\n')
-		{
-                	getline(file,line);
-    		    	R[index].push_back(line);
-		}
-            	else
-		{
-			getline(file,line);
-                	index += 1;
-			if(index==DBLPWW)
-				break;
-		}
+		    if(file.peek()!='\n')
+		    {
+                getline(file,line);
+    		    R[index].push_back(line);
+		    }
+            else
+		    {
+			    getline(file,line);
+                index += 1;
+			    if(index==DBLPWW)
+				    break;
+		    }
         }  
     }
     file.close();
@@ -76,15 +96,22 @@ if(experiment==t1)
 
     for(int i=0;i<R.size();i++) // for all r in R do    //can change the size to DBLP-WW possibly if not exact # of records
     {
-        for(int j=0;j<out.C.size();j++)  // for all C in C do
-        {
-        	cluster_similarity(R[i],out.C[j]);
-        }
+        for(int j=0;j<C.size();j++)  // for all C in C do
+            storage += cluster_similarity(R[i],Sc[j]);
+	
+	if(storage!=0)
+		cout << storage / Sc.size();
+	if(i==1)
+		break;
+	
+    }
+
+/*
         if(//maxC in csims(r,C)>=0s   if true, then merge to the next possible cluster)
         {
             //Cmax = arg max C in c sims(r,C)
             Cmax.push_back(R[i]);           //Cmax <- Cmax union r          append r to Cmax
-            //r1 <- arg max r in C r.t      O(1) since sorted by time
+            //r1 <- arg max r in C r.t     O(1) since sorted by time
             Scmax.pop_back();                //sCmax <- sCmax \ r1           remove the last record
             Scmax.push_back(R[i]);          //sCmax <- sCmax unior r        append r as the last record
         }
@@ -92,37 +119,14 @@ if(experiment==t1)
         {      
             Cmax = R[i];           // create a new cluster             cmax = {R}
             Scmax = R[i];           // sCmax = {r}          // initialize the signature for Cmax
-            out.C.push_back(Cmax);                       // C <- C union Cmax
-            out.Sc.push_back(Scmax);                       // Sc <- Sc union sCmax
+            out.C[i].push_back(Cmax);                       // C <- C union Cmax
+            out.Sc[i].push_back(Scmax);                       // Sc <- Sc union sCmax
         }   
     }
     return out;       // return C, Sc
-
+*/
 return 0;
 }
-
-double cluster_similarity(vector<string> r, int c, output out)
-    int num_SC = sizeof(out.Sc);
-    double result = 0;
-    for(int r2 = 0; r2 < num_SC; r2++){  //for every r' in SC
-        result += record_similarity(r, r2); //compute record_sim(r, r')
-    return result / num_SC;                 //return average (result divided by num of signatures)
-        
-    }
-}
-
-double record_similarity(int r1, int r2){
-    double result = 0;
-    for(int a = 0; a < r.size(); a++)    //for every attribute a in A
-        //result += compute sima(r, r')
-    return result / r.size();               //divide computed average by number of attributes
-
-}
-
-
-
-
-
 // -------------------------------------------------------------------------------------------------------------------
 // Run-Time Experiment
 /*
@@ -130,6 +134,7 @@ else if(experiment==t2)
 {
     cout << "Enter '0' if you would like to do 10% of the DBLP-100K dataset or '1' for its entirety: ";
     cin >> type;
+
     if(type==0)
     {
         vector<vector<string> > R((DBLP100K/10));
@@ -145,14 +150,14 @@ else if(experiment==t2)
                 if(file.peek()!='\n')
                 {
                 	getletline(file,line);
-			R[index].push_back(line);    
+			        R[index].push_back(line);    
                 }
                 else
                 {
                 	index += 1;
-			getline(file,line);
-			if(index==(DBLP100K/10))
-				break;
+			        getline(file,line);
+			        if(index==(DBLP100K/10))
+				        break;
                 }
             }   
         }   
@@ -168,30 +173,32 @@ else if(experiment==t2)
             cout << "Unable to open file" << endl;
         else
         {
-		while(!file.eof())
-            	{
-			if(file.peek()!='\n')
+            while(!file.eof())
+            {
+		        if(file.peek()!='\n')
 		        {
-                		getline(file,line);
+                	getline(file,line);
 			        R[index].push_back(line);
-                	}
-                	else
-                	{
-                		index += 1;
-				getline(file,line);
-				if(index==DBLP100K)
-					break;
-                	}
-            	}
+                }
+                else
+                {
+                	index += 1;
+			        getline(file,line);
+			        if(index==DBLP100K)
+				        break;
+                }
+            }
         }   
         file.close();
     }
+
     beg = clock();
     end = clock();
+
     elapsedtime = (end-beg) / (unsigned long long int)CLOCKS_PER_SEC;       // might be a double & its gonna give seconds i think
     cout << "Scalability experiments on 10% of DBLP-100K had an execution time of about: "<< elapsedtime << " hours" << endl;
+
     return out;
 }
- 
+*/ 
 }
-*/
