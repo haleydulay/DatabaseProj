6d726bac3949fe70da17ee705ddff418272284eb
haleydulay <43179228+haleydulay@users.noreply.github.com>
2019-12-02 20:53:22 -0500

    Add files via upload

--- algorithm1.cpp
+++ algorithm1.cpp
@@ -14,6 +14,7 @@ Sc, the set of cluster signatures corresponding to C, each cluster in C has a si
 #include <string>
 #include <ctime>
 #include <cstring>
+#include <algorithm>
 #define DBLPWW 738
 #define DBLP100K 103059
 
@@ -56,9 +57,16 @@ result += record_similarity(r,r2,j);
 	return result;
 }
 
+double dynamic_similarity(vector<string> r, vector<string> r2, int j)
+{
+double result = 0;
+
+}
+
 int main()
 {
-double threshold = 1000000000;
+int identifier = 0;
+double threshold = 0.2;
 ifstream file;
 int index = 0;
 clock_t beg = 0;
@@ -72,8 +80,10 @@ string t2 = "Run-Time";
 vector<string> Cmax;
 vector<string> Scmax;
 double storage = 0;
-vector<vector<string> > C(738);
-vector<vector<string> > Sc(738);
+vector<vector<string> > C(DBLPWW);
+vector<vector<string> > Sc(DBLPWW);
+vector<vector<string> > D(DBLPWW);
+vector<vector<string> > Dc(DBLPWW);
 
 
 cout << "Input either 'Matching-Accuracy' or 'Run-Time' as the experiment you want to conduct: ";
@@ -92,15 +102,15 @@ if(experiment==t1)
     {
         while(!file.eof())
         {
-		    if(file.peek()!='\n')
+		    if(file.peek()!=' ')
 		    {
-                getline(file,line);
+                    getline(file,line);
     		    R[index].push_back(line);
 		    }
-            else
+            	    else
 		    {
 			    getline(file,line);
-                index += 1;
+                	    index += 1;
 			    if(index==DBLPWW)
 				    break;
 		    }
@@ -108,52 +118,50 @@ if(experiment==t1)
     }
     file.close();
 
-/*
-    C[0].push_back("Feng Li");
-  C[0].push_back("Feng Li");
-counter++;
-counter++;
-counter ++;
-counter ++;
- Sc[0].push_back("Feng Li");
- Sc[0].push_back("Feng Lu");
-C[1].push_back("Feng Li");
-C[1].push_back("Feng Li");
-Sc[1].push_back("Sheldon Park");
-Sc[1].push_back("Wei Wang"); */
-
-
+// ------------------------------------------------ Algorithm 1 ---------------------------------------------------------------
     for(int i=0;i<R.size();i++) // for all r in R do    //can change the size to DBLP-WW possibly if not exact # of records
     {
     double test = 0;
-        for(int j=0;j<counter-1;j++)  	// for all C in C do
+        for(int j=0;j<=counter-1;j++)  	// for all C in C do
             storage += cluster_similarity(R[i],Sc[j],j);
-  
-	//if(i==1)
-	//break;
 
 	test = cluster_similarity(R[i],Sc[placement],i);
 
-	cout << test << endl;	
-
-        if(test>=threshold)
+        if(test>=threshold)	// recall threshold
         {
             //Cmax = arg max C in c sims(r,C) = placement
             for(int k=0;k<R[i].size();k++)
 	    	C[placement].push_back(R[i][k]);           //Cmax <- Cmax union r          append r to Cmax
+	    sort(C[placement].begin(), C[placement].end());
+	    C[placement].erase(unique(C[placement].begin(),C[placement].end()),C[placement].end());
             //r1 <- arg max r in C r.t     O(1) since sorted by time
             Sc[placement].pop_back();                //sCmax <- sCmax \ r1           remove the last record
             Sc[placement].push_back(R[i][R[i].size()-1]);          //sCmax <- sCmax union r        append r as the last record
         }
         else
-        {      
-            Cmax = R[i];           // create a new cluster             cmax = {R}
-            Scmax = R[i];           // sCmax = {r}          // initialize the signature for Cmax
-            C[i].push_back(Cmax);                       // C <- C union Cmax
-            Sc[i].push_back(Scmax);                       // Sc <- Sc union sCmax
-        }   
+        {
+            for(int k=0;k<R[i].size();k++)      
+            	Cmax.push_back(R[i][k]);           // create a new cluster             cmax = {R}
+	    Scmax.push_back(R[i][0]);
+            Scmax.push_back(R[i][R[i].size()-1]);           // sCmax = {r}          // initialize the signature for Cmax
+	    for(int k=0;k<R[i].size();k++)
+            	C[identifier].push_back(Cmax[k]);                       // C <- C union Cmax
+            Sc[identifier].push_back(Scmax[0]);                       // Sc <- Sc union sCmax
+	    Sc[identifier].push_back(Scmax[1]);
+	    identifier++;
+            counter++;
+        }
+	Cmax.clear();
+	Scmax.clear();  
     }
 
+// ----------------------------------------------- Algorithm 2 -----------------------------------------------------------------
+/*for(int i=0;i<counter-1;i++)
+{
+	for(int j=0;j<=counter-1;j++)
+		storage = dynamic_similarity(Sc[i],D[j],j);
+	if(
+}*/
 
 return 0;
 }
