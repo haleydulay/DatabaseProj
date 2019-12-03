d87250b44b07329b30a73617505b5e26fafdda20
haleydulay <43179228+haleydulay@users.noreply.github.com>
2019-12-02 19:37:30 -0500

    update with more psuedocode

---
+++ algorithm2.cpp
@@ -0,0 +1,40 @@
+
+/*
+Input:
+C, the clustering result of the S-phase.
+0s, a threshold to determine whether to merge two records.
+Output:
+D, the final clustering result
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+vector<string> D; // D={}
+
+for(int i=0;i<clusters.size();i++)
+{
+  Sc = {sC}   // pack its signature into a signature set
+  for(int j=0;j<D.size();j++)
+   {
+   //compute simd(C,D)    dynamic similarity
+   }
+   if(//maxD >= threshold)
+   {
+       Dmax = maxD
+       Dmax = C U Dmax  //merge to the best
+       SDmax = Sc U SDmax   //update the signature
+
+   }
+   else{
+       D = D U C    //keep the original cluster
+   }
+}
+return D;
+
+return 0;   // return D
+}
