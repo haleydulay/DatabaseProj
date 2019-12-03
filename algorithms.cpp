2ddc71e2c5e869f625b814ce0f0d8265e01f0c26
haleydulay <43179228+haleydulay@users.noreply.github.com>
2019-12-03 15:05:54 -0500

    Add files via upload

--- algorithms.cpp
+++ algorithms.cpp
@@ -124,10 +124,10 @@ string t2 = "Run-Time";
 vector<string> Cmax;
 vector<string> Scmax;
 double storage = 0;
-vector<vector<string> > C(DBLPWW);
-vector<vector<string> > Sc(DBLPWW);
-vector<vector<string> > D(DBLPWW);
-vector<vector<string> > Dc(DBLPWW);
+vector<vector<string> > C(DBLP100K);
+vector<vector<string> > Sc(DBLP100K);
+vector<vector<string> > D(DBLP100K);
+vector<vector<string> > Dc(DBLP100K);
 
 
 cout << "Input either 'Matching-Accuracy' or 'Run-Time' as the experiment you want to conduct: ";
@@ -297,7 +297,7 @@ return 0;
 
 // -------------------------------------------------------------------------------------------------------------------
 // Run-Time Experiment
-/*
+
 else if(experiment==t2)
 {
     cout << "Enter '0' if you would like to do 10% of the DBLP-100K dataset or '1' for its entirety: ";
@@ -360,13 +360,109 @@ beg = clock();
             }
         }   
         file.close();
+
+        // ------------------------------------------------ Algorithm 1 ---------------------------------------------------------------
+    for(int i=0;i<R.size();i++) // for all r in R do    //can change the size to DBLP-WW possibly if not exact # of records
+    {
+    double test = 0;
+        for(int j=0;j<counter-1;j++)  	// for all C in C do
+            storage += cluster_similarity(R[i],Sc[j],j);
+
+	test = cluster_similarity(R[i],Sc[placement],i);
+
+        if(test>=threshold)	// recall threshold
+        {
+            //Cmax = arg max C in c sims(r,C) = placement
+            for(int k=0;k<R[i].size();k++)
+	    	C[placement].push_back(R[i][k]);           //Cmax <- Cmax union r          append r to Cmax
+	    sort(C[placement].begin(), C[placement].end());
+	    C[placement].erase(unique(C[placement].begin(),C[placement].end()),C[placement].end());
+            //r1 <- arg max r in C r.t     O(1) since sorted by time
+            Sc[placement].pop_back();                //sCmax <- sCmax \ r1           remove the last record
+            Sc[placement].push_back(R[i][R[i].size()-1]);          //sCmax <- sCmax union r        append r as the last record
+        }
+        else
+        {
+            for(int k=0;k<R[i].size();k++)      
+            	Cmax.push_back(R[i][k]);           // create a new cluster             cmax = {R}
+	    Scmax.push_back(R[i][0]);
+            Scmax.push_back(R[i][R[i].size()-1]);           // sCmax = {r}          // initialize the signature for Cmax
+	    for(int k=0;k<R[i].size();k++)
+            	C[identifier].push_back(Cmax[k]);                       // C <- C union Cmax
+            Sc[identifier].push_back(Scmax[0]);                       // Sc <- Sc union sCmax
+	    Sc[identifier].push_back(Scmax[1]);
+	    identifier++;
+            counter++;
+        }
+	Cmax.clear();
+	Scmax.clear();  
     }
 
-    end = clock();
+cout << "Would you like to view the static phase's clustering result? 0 for No and 1 for Yes: ";
+cin >> choice;
 
-time_taken = double(end-beg) / double(CLOCKS_PER_SEC);
+if(choice==1)
+{
+        for(int i=0;i<counter-1;i++)
+        {
+                for(int j=0;j<C[i].size();j++)
+                        cout << C[i][j] << ", ";
+                cout << endl;
+        }
+cout << endl << "Total clusters are now: " << counter << " instead of 738 original records" << endl;
+}
+
+cout << endl << "Would you like to view the signatures of the static phase's clustering result? 0 for No and 1 for Yes: ";
+cin >> choice;
+
+if(choice==1)
+{
+        for(int i=0;i<counter-1;i++)
+        {
+                for(int j=0;j<Sc[i].size();j++)
+                        cout << Sc[i][j] << ", ";
+                cout << endl;
+        }
+}
+
+
+
+// ----------------------------------------------- Algorithm 2 -----------------------------------------------------------------
+for(int i=0;i<counter-1;i++)		// for all C in C do
+{
+double test = 0;
+	for(int j=0;j<dcount-1;j++)		// for all D in D do
+		storage += dynamic_similarity(C[i],Dc[j],j);		// dynamic similarity
+	
+	test = dynamic_similarity(C[i],Dc[location],i);		// best possible cluster based off dynamic threshold
+
+	if(test>=d_threshold)
+	{
+		//Dmax = arg max D in D simd(C,D) = location
+		for(int k=0;k<C[i].size();k++)
+			D[location].push_back(C[i][k]);		// merge to the best 
+		sort(D[location].begin(),D[location].end());
+		D[location].erase(unique(D[location].begin(),D[location].end()),D[location].end());
+		Dc[location].pop_back();
+		Dc[location].push_back(C[i][C[i].size()-1]);	
+	}
+	else
+	{
+		for(int k=0;k<C[i].size();k++)
+			D[setting].push_back(C[i][k]);	// keep the original cluster
+		Dc[setting].push_back(Sc[i][0]);
+		Dc[setting].push_back(Sc[i][Sc[i].size()-1]);
+		dcount++;
+		setting++;
+	}
+    }
+}
+
+end = clock();
+
+double time_taken = double(end-beg) / double(CLOCKS_PER_SEC);
 cout << endl << "Scalability experiments had an execution time of about: "<< time_taken << " seconds" << endl;
 
 }
-*/ 
+    return 0;
 }
