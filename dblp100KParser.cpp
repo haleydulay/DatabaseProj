14f12bd5f3f32f99fb2b53c2b2628fd879c75712
haleydulay <43179228+haleydulay@users.noreply.github.com>
2019-12-03 15:28:43 -0500

    Add files via upload

--- dblp100KParser.cpp
+++ dblp100KParser.cpp
@@ -8,6 +8,7 @@
 #include <typeinfo>
 #include "/Users/haley/Desktop/rapidxml.hpp"
 
+
 using namespace rapidxml;
 using namespace std;
 
@@ -22,31 +23,29 @@ int main(){
     doc.parse<0>(&buffer[0]);
     xml_node<> * root_node = doc.first_node("dblp");
 
-    int counter = 0;    //keep track of the number of records
+    int counter = 0;
+
 
     //iterate over the dblp article entries
     for(xml_node<>* article_node = root_node->first_node(); article_node; article_node = article_node->next_sibling())
     {   
-    
-        for(xml_node<> * title_node= article_node->first_node(); title_node; title_node = title_node->next_sibling())
-        {   
-            if(counter >= 100000){
-                break;
-            }
-            
-            counter += 1;
-            char* type = title_node->name();
-            if(strcmp(type, "author") == 0){
-                cout << title_node->value() << endl;
-            }
-            else if(strcmp(type, "title") == 0){
-                cout << title_node->value() << endl;
-            }
-                //else if(strcmp(type, "")) conference ?
-            
 
+          
+            for(xml_node<> * found_node= article_node->first_node("author"); found_node; found_node = found_node->next_sibling()){
+                if(counter == 2000000){
+                    break;
+                    }
+                counter += 1;
+                char* type = found_node->name();
+               if(strcmp(type,"author") == 0){
+                    cout << found_node->value() << endl;
+                }
+
+            }
+            cout << "\n";
         }
-        cout << "\n";
-    }
-    cout << "number of records: " << counter << endl;
-}   
+    
+        cout << "number of records: " << counter << endl;
+}
+
+    
